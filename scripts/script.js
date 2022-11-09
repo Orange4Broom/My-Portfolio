bfb472aa6d1bc80bd71fe0f1eeb5a599598acb15
Orange4Broom <orang4broom@gmail.com>
2022-11-09 21:42:27 +0100

    Create navigation/make it responsive and hidding

---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+const header = document.querySelector('#navigation');
+document.addEventListener('scroll', () => {
+    header.classList[
+        window.scrollY > 0
+        ? 'add'
+        : 'remove'
+    ]('sticky');
+});
+
+const showButton = document.querySelector('#menuButton');
+const homeButton = document.querySelector('#homeButton');
+const aboutButton = document.querySelector('#aboutButton');
+const contactButton = document.querySelector('#contactButton');
+const menu = document.querySelector('#menu');
+menu.classList.add('hide');
+let click = 0;
+
+showButton.onclick = function () {
+        menu.classList[click%2==0 ?'remove' :'add']('hide');
+    click++;
+}
+
+homeButton.onclick = function () {
+        menu.classList[click%2==0 ?'remove' :'add']('hide');
+    click++;
+}
+
+aboutButton.onclick = function () {
+    menu.classList[click%2==0 ?'remove' :'add']('hide');
+click++;
+}
+
+contactButton.onclick = function () {
+    menu.classList[click%2==0 ?'remove' :'add']('hide');
+click++;
+}
