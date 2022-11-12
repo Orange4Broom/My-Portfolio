f4067442c8d92c42a14d916a5116cd5af6d75c70
Orange4Broom <orang4broom@gmail.com>
2022-11-12 22:42:00 +0100

    Add open navigatoin animation

--- scripts/script.js
+++ scripts/script.js
@@ -6,31 +6,3 @@ document.addEventListener('scroll', () => {
         : 'remove'
     ]('sticky');
 });
-
-const showButton = document.querySelector('#menuButton');
-const homeButton = document.querySelector('#homeButton');
-const aboutButton = document.querySelector('#aboutButton');
-const contactButton = document.querySelector('#socialButton');
-const menu = document.querySelector('#menu');
-menu.classList.add('hide');
-let click = 0;
-
-showButton.onclick = function () {
-        menu.classList[click%2==0 ?'remove' :'add']('hide');
-    click++;
-}
-
-homeButton.onclick = function () {
-        menu.classList[click%2==0 ?'remove' :'add']('hide');
-    click++;
-}
-
-aboutButton.onclick = function () {
-    menu.classList[click%2==0 ?'remove' :'add']('hide');
-click++;
-}
-
-contactButton.onclick = function () {
-    menu.classList[click%2==0 ?'remove' :'add']('hide');
-click++;
-}
